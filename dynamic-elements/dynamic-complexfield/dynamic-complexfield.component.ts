5c9b98595b213f98f674ca674134b8b0be4f7466
LAG David Hodel <david.hodel@leuchterag.ch>
2020-05-04 15:00:58 +0200

    complexfield handles default value

--- dynamic-elements/dynamic-complexfield/dynamic-complexfield.component.ts
+++ dynamic-elements/dynamic-complexfield/dynamic-complexfield.component.ts
@@ -2,11 +2,11 @@ import { Component, forwardRef, OnInit, ChangeDetectorRef, AfterViewInit, ViewCh
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { startWith, map, concat, debounceTime } from 'rxjs/operators';
+import { startWith, map, debounceTime } from 'rxjs/operators';
 
 import { AbstractControlValueAccessor } from '../abstract-control-value-accesor';
 import { DataProvider } from '../../services/data-provider';
-import { MatAutocomplete, MatChipInputEvent, MatAutocompleteSelectedEvent, MatAutocompleteTrigger, MatInput } from '@angular/material';
+import { MatAutocompleteSelectedEvent, MatAutocompleteTrigger } from '@angular/material';
 
 export const COMPLEXFIELD_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -20,7 +20,7 @@ export const COMPLEXFIELD_CONTROL_VALUE_ACCESSOR: any = {
     DataProvider
   ],
   selector: 'td-dynamic-complexfield',
-  styleUrls: [ './dynamic-complexfield.component.scss' ],
+  styleUrls: ['./dynamic-complexfield.component.scss'],
   templateUrl: './dynamic-complexfield.component.html',
 })
 export class TdDynamicComplexfieldComponent extends AbstractControlValueAccessor implements ControlValueAccessor, OnInit, AfterViewInit {
@@ -55,17 +55,23 @@ export class TdDynamicComplexfieldComponent extends AbstractControlValueAccessor
   icon: string = "image";
   functionUrl: string = undefined;
   error: any = undefined;
-  
+
   constructor(dataProvider: DataProvider, private cdRef: ChangeDetectorRef) {
     super();
     this.data_provider = dataProvider;
   }
-  
+
   ngAfterViewInit() {
     this.control = new FormControl();
   }
 
   ngOnInit(): void {
+    const defaultValue = this.control.value;
+    if (defaultValue) {
+      this.filter(defaultValue).subscribe(val => {
+        this.selectedObject = val.find(e => e.key === defaultValue);
+      });
+    }
   }
 
   onClickInput() {
@@ -77,28 +83,28 @@ export class TdDynamicComplexfieldComponent extends AbstractControlValueAccessor
       startWith(''),
       debounceTime(300)
     ).subscribe(val => {
-        console.log('control changed')
-        this.loadingData = true;
-        this.ignoreScroll = true;
-        this.skip = 0;
+      console.debug('control changed');
+      this.loadingData = true;
+      this.ignoreScroll = true;
+      this.skip = 0;
 
-        if(!(val instanceof DynamicObject)){ // ignore valuechange caused by the selection of a searched item
-          this.loadData(val.toString());
-        }
-      })
+      if (!(val instanceof DynamicObject)) { // ignore valuechange caused by the selection of a searched item
+        this.loadData(val.toString());
+      }
+    });
   }
 
   loadData(val: string) {
     this.error = undefined;
     this.filter(val)
       .subscribe(res => {
-        console.debug('form control values: ', res)
+        console.debug('form control values: ', res);
         this.objects = new Array<any>();
         res.forEach(element => {
-          if(!this.objects.find(o => o.key === element.key)) {
+          if (!this.objects.find(o => o.key === element.key)) {
             this.objects.push(element);
           }
-        })
+        });
         this.detectChanges();
         this.loadingData = false;
         this.ignoreScroll = false;
@@ -108,83 +114,83 @@ export class TdDynamicComplexfieldComponent extends AbstractControlValueAccessor
         this.ignoreScroll = false;
         this.error = error;
         this.detectChanges();
-      })
+      });
   }
 
   detectChanges() {
     this.cdRef.detectChanges();
     this.cdRef.reattach();
   }
-  
+
   // load next 5 objects
   onScroll(event) {
-    if(!this.ignoreScroll){
+    if (!this.ignoreScroll) {
       let scrollTop = event.target.scrollTop;
       let scrollHeight = event.target.scrollHeight;
       let childElementCount = event.target.childElementCount;
 
-      if((scrollHeight / childElementCount) == scrollTop){
-        console.debug('can load more')
+      if ((scrollHeight / childElementCount) == scrollTop) {
+        console.debug('can load more');
         this.loadingData = true;
         this.loadMore(event);
       }
     }
   }
-  
+
   loadMore(event) {
-    console.debug('loading 5 more')
+    console.debug('loading 5 more');
     this.skip += 5;
 
     this.filter()
-    .subscribe(res => {
-      res.forEach(element => {
-        if(!this.objects.find(o => o.key === element.key)) {
-          this.objects.push(element);
-        }
-      })
-      
-      this.loadingData = false;
-    }, (error) => {
-      console.warn(error);
-    })
+      .subscribe(res => {
+        res.forEach(element => {
+          if (!this.objects.find(o => o.key === element.key)) {
+            this.objects.push(element);
+          }
+        });
+
+        this.loadingData = false;
+      }, (error) => {
+        console.warn(error);
+      });
   }
 
   filter(term: string = ""): Observable<any[]> {
     return this.data_provider.fetchData(this.functionUrl, this.source, !term || typeof term !== 'string' ? "" : term, this.skip, this.take)
       .pipe(
         map(response => {
-            let dynObjArray = new Array<DynamicObject>();
-            console.debug(`response of function ${this.source}: `, response);
-            if (response['results']) {
-              response['results'].forEach(result => {
-                let obj = new DynamicObject();
+          let dynObjArray = new Array<DynamicObject>();
+          console.debug(`response of function ${this.source}: `, response);
+          if (response['results']) {
+            response['results'].forEach(result => {
+              let obj = new DynamicObject();
 
-                // load title and subtitle with right culture
-                let stringArr = this.titlefield.split('.');
-                let object = result[stringArr[0]];
-                let culturekey = stringArr[1];
-                let title = culturekey && object[culturekey] ? object[culturekey] : object;
+              // load title and subtitle with right culture
+              let stringArr = this.titlefield.split('.');
+              let object = result[stringArr[0]];
+              let culturekey = stringArr[1];
+              let title = culturekey && object[culturekey] ? object[culturekey] : object;
 
-                stringArr = this.subtitlefield.split('.');
-                object = result[stringArr[0]];
-                culturekey = stringArr[1];
-                let subtitle = culturekey && object[culturekey] ? object[culturekey] : object;
+              stringArr = this.subtitlefield.split('.');
+              object = result[stringArr[0]];
+              culturekey = stringArr[1];
+              let subtitle = culturekey && object[culturekey] ? object[culturekey] : object;
 
-                // map response elements
-                obj.keyfield = response['key'];
-                obj.key = result[response['key']];
-                obj.name = result['name'];
-                obj.titlefield = title;
-                obj.subtitlefield = subtitle;
-                obj.required = response['required'];
+              // map response elements
+              obj.keyfield = response['key'];
+              obj.key = result[response['key']];
+              obj.name = result['name'];
+              obj.titlefield = title;
+              obj.subtitlefield = subtitle;
+              obj.required = response['required'];
 
-                dynObjArray.push(obj)
-              });
+              dynObjArray.push(obj);
+            });
 
-              return dynObjArray;
-            }
+            return dynObjArray;
+          }
         })
-      )
+      );
   }
 
   displayView(object?: any): string | undefined {
@@ -192,7 +198,7 @@ export class TdDynamicComplexfieldComponent extends AbstractControlValueAccessor
   }
 
   optionSelected(event: MatAutocompleteSelectedEvent): void {
-    if(event && event.option) {
+    if (event && event.option) {
       this.selectedObject = event.option.value;
     }
   }
@@ -201,8 +207,8 @@ export class TdDynamicComplexfieldComponent extends AbstractControlValueAccessor
     this.selectedObject = undefined;
     this.loadData('');
     setTimeout(() => {
-      this.autocompleteTrigger.openPanel()
-    }, 0)
+      this.autocompleteTrigger.openPanel();
+    }, 0);
   }
 }
 
